070bd2dc40374318a3f99bec8aaad78d25d2aa84
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-13 20:07:11 -0800

    Excellent Descriptions

---
+++ program1.py
@@ -0,0 +1,26 @@
+# step 1. Ask for Grade Percentage, covet and store
+grade = int(input("What is your grade percentage?: "))
+
+# grade/mark    percentage     description
+
+# 1.0           97-100          Excellent
+if grade >= 97 and grade <= 100:
+    print("mark: 1.o")
+    print("Description: Excellent")
+
+# 1.25          94-96           Excellent
+elif grade >= 94 and grade <= 96:
+    print("mark: 1.25")
+    print("Description: Excellent ")
+    
+# 1.5           91-93           Very Good
+# 1.75          88-90           Very Good
+# 2.0           85-87           Good
+# 2.25          82-84           Good
+# 2.50          79-81           Satisfactory
+# 2.75          76-78           Satisfactory
+# 3.0           75              Passing
+# 5.0           65-74           Failure
+# Inc.                          Incomplete
+# W                             Withdrawn
+# D                             Dropeed
